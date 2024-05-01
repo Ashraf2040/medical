f401ec6c8c6e32e746c1a37bd9c4778b0a78666d
Ashraf2040 <m_a199025@yahoo.com>
2024-05-02 00:33:40 +0300

    adding slider

---
+++ app/healthcare/info/[id]/page.jsx
@@ -0,0 +1,258 @@
+
+"use client";
+
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+
+import './styles.css';
+
+// import required modules
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+
+
+export default function TopicInfo({params}) {
+  const dataCards = [
+    { 
+      src :"/1.jpg",
+      title: " Hazards in the Workplace",
+     },
+    { 
+      src :"/2.jpg",
+      title: "Hygiene in the Workplace",
+     },
+    { 
+      src :"/3.jpg",
+      title: "Cybersecurity Fundamentals",
+     },
+  ]
+  return (
+    <div className='flex flex-col gap-2 justify-center items-center w-full'>
+    <h1 className='   text-[#02B1BF] mt-6 w-fit   bg-[#dbeefd] rounded-lg p-3'>{dataCards[params.id].title}</h1>
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// "use client"
+
+// import Link from 'next/link'
+
+// import React, { useRef, useState } from 'react';
+// // Import Swiper React components
+// import { Swiper, SwiperSlide } from 'swiper/react';
+
+// // Import Swiper styles
+// import 'swiper/css';
+// import 'swiper/css/pagination';
+
+// import './styles.css';
+
+// // import required modules
+// import { Pagination } from 'swiper/modules';
+// import Image from 'next/image';
+// export default function InfoPage({params}) {
+ 
+//       return (
+//         <div className='px-4 flex flex-col  '>
+//           <Link href="/healthcare">
+//           <button className="bg-gradient-to-br px-2 rounded-lg font-semibold text-white py-2 from-[#54A15E] to-green-400 my-2">
+//           Back {params.id}
+//           </button>
+//         </Link>
+//           <Swiper pagination={true} modules={[Pagination]} className="mySwiper w-4/5">
+//             <SwiperSlide><Image src="/a.jpg" alt="diaf" width={180} height={180}  className="h-10 w-10" /></SwiperSlide>
+//             <SwiperSlide><Image src="/b.jpg" alt="diaf" width={180} height={180}  className="h-10 w-10" /></SwiperSlide>
+//             <SwiperSlide><Image src="/c.jpg" alt="diaf" width={180} height={180}  className="h-10 w-10" /></SwiperSlide>
+//             <SwiperSlide><Image src="/d.jpg" alt="diaf" width={180} height={180}  className="h-10 w-10" /></SwiperSlide>
+//             <SwiperSlide><Image src="/e.jpg" alt="diaf" width={180} height={180}  className="h-10 w-10" /></SwiperSlide>
+//             <SwiperSlide><Image src="/f.jpg" alt="diaf" width={180} height={180}  className="h-10 w-10" /></SwiperSlide>
+         
+//           </Swiper>
+//         </div>
+//       );
+//     }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div className='p-8'>
+<h1 className='bg-black font-bold py-2 px-8 text-center text-white'>HealthCare Title Info</h1>
+<p className='my-8 text-xl'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates, porro, quas dolorem pariatur deserunt quo culpa molestiae perspiciatis ratione, explicabo iusto sapiente sunt odio. Quae modi adipisci voluptates doloribus accusantium voluptate, eveniet odio ab error dolor quia, consequatur assumenda enim minima velit aliquam. Nihil, rem voluptates ea perferendis est magnam!</p>
+<Link href="/medical"><button className='bg-black font-bold py-2 px-8 text-center rounded-lg text-white'>Back to Cards</button></Link>
+</div> */}
--- app/healthcare/page.jsx
+++ app/healthcare/page.jsx
@@ -1,42 +1,74 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+
+import './styles.css';
+
+// import required modules
+import { EffectCards } from 'swiper/modules';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const dataCards = [
+  { 
+    src :"/1.jpg",
+    title: " Hazards in the Workplace",
+   },
+  { 
+    src :"/2.jpg",
+    title: "Hygiene in the Workplace",
+   },
+  { 
+    src :"/3.jpg",
+    title: "Cybersecurity Fundamentals",
+   },
+]
 export default function HealthCare() {
-
-  const dataCards = [
-    { 
-      src :"/1.jpg",
-      title: " Hazards in the Workplace",
-     },
-    { 
-      src :"/2.jpg",
-      title: "Hygiene in the Workplace",
-     },
-    { 
-      src :"/3.jpg",
-      title: "Cybersecurity Fundamentals",
-     },
-  ]
+  
   return (
-    <div className='flex justify-center items-center flex-col py-4'>
-       <Link href="/">
-          <button className="bg-gradient-to-br px-2 rounded-lg font-semibold text-white py-2 from-[#54A15E] to-green-400 my-2">
-          Back to Home
-          </button>
-        </Link>
+    <div className=' flex flex-col w-full  '>
+      <h1 className='text-lg mb-6 text-[#02B1BF] text-center font-semibold'>Choose topic for more info...</h1>
+      <Swiper
+        effect={'cards'}
+        grabCursor={true}
+        modules={[EffectCards]}
+        className="mySwiper w-full"
+      >
+        {dataCards.map((item, index) => (
 
-        <div className='flex justify-center items-center flex-col gap-4'>
-      {dataCards.map((card) =>
-      
-      <Link href='/medical/info' key={card.title} className=' shadow-md shadow-black rounded-lg relative border-black h-fit'>
-        <Image src={card.src} alt='a' width={300} height={300} className='relative'/> 
-        <p className=' text-md font-semibold flex justify-around items-center  p-3 py-6  bg-white w-full absolute bottom-0'>{card.title} <span className='flex w-10 items-center gap-1 text-lg opacity-60'>6 <Image src="/icon.jpg" alt='a' width={40} height={20} className="" /></span></p></Link> 
-    )}
-        </div>
-     
+          
+          <SwiperSlide key={index} className='w-full relative'>
+            <Link href={`/healthcare/info/${index}`} className='w-full'>
+            <Image
+              src={item.src}
+              alt="diaf"
+              width={600}
+              height={600}
+              className="w-full h-full"
+            />
+            <span className='bg-[#02B1BF] absolute top-0 right-0 px-4 py-1'>{index+1} of {dataCards.length}</span>
+            <p className="text-md text-center w-full py-6 font-semibold absolute bottom-0 bg-white text-[#02B1BF]">{item.title}</p>
+            </Link>
+            
+          </SwiperSlide>
+        ))}
+        {/* <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide> */}
+      </Swiper>
     </div>
-  )
+  );
 }
 
 
@@ -54,6 +86,46 @@ export default function HealthCare() {
 
 
 
+// import Image from 'next/image'
+// import Link from 'next/link'
+// import React from 'react'
+
+// export default function HealthCare() {
+
+//   const dataCards = [
+//     { 
+//       src :"/1.jpg",
+//       title: " Hazards in the Workplace",
+//      },
+//     { 
+//       src :"/2.jpg",
+//       title: "Hygiene in the Workplace",
+//      },
+//     { 
+//       src :"/3.jpg",
+//       title: "Cybersecurity Fundamentals",
+//      },
+//   ]
+//   return (
+//     <div className='flex justify-center items-center flex-col py-4'>
+//        <Link href="/">
+//           <button className="bg-gradient-to-br px-2 rounded-lg font-semibold text-white py-2 from-[#54A15E] to-green-400 my-2">
+//           Back to Home
+//           </button>
+//         </Link>
+
+//         <div className='flex justify-center items-center flex-col gap-4'>
+//       {dataCards.map((card) =>
+      
+//       <Link href='/medical/info' key={card.title} className=' shadow-md shadow-black rounded-lg relative border-black h-fit'>
+//         <Image src={card.src} alt='a' width={300} height={300} className='relative'/> 
+//         <p className=' text-md font-semibold flex justify-around items-center  p-3 py-6  bg-white w-full absolute bottom-0'>{card.title} <span className='flex w-10 items-center gap-1 text-lg opacity-60'>6 <Image src="/icon.jpg" alt='a' width={40} height={20} className="" /></span></p></Link> 
+//     )}
+//         </div>
+     
+//     </div>
+//   )
+// }
 
 
 
@@ -98,11 +170,27 @@ export default function HealthCare() {
 
 
 
-{/* <div className='  w-full flex justify-center items-center flex-wrap gap-10'>
-<Link href='/medical/info'><Image src="/a.jpg" alt='a' width={300} height={300} /></Link> 
-<Link href='/medical/info'><Image src="/b.jpg" alt='a' width={300} height={300} /></Link> 
-<Link href='/medical/info'><Image src="/c.jpg" alt='a' width={300} height={300} /></Link> 
-<Link href='/medical/info'><Image src="/d.jpg" alt='a' width={300} height={300} /></Link> 
-<Link href='/medical/info'><Image src="/e.jpg" alt='a' width={300} height={300} /></Link> 
-<Link href='/medical/info'><Image src="/f.jpg" alt='a' width={300} height={300} /></Link> 
-</div> */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// {/* <div className='  w-full flex justify-center items-center flex-wrap gap-10'>
+// <Link href='/medical/info'><Image src="/a.jpg" alt='a' width={300} height={300} /></Link> 
+// <Link href='/medical/info'><Image src="/b.jpg" alt='a' width={300} height={300} /></Link> 
+// <Link href='/medical/info'><Image src="/c.jpg" alt='a' width={300} height={300} /></Link> 
+// <Link href='/medical/info'><Image src="/d.jpg" alt='a' width={300} height={300} /></Link> 
+// <Link href='/medical/info'><Image src="/e.jpg" alt='a' width={300} height={300} /></Link> 
+// <Link href='/medical/info'><Image src="/f.jpg" alt='a' width={300} height={300} /></Link> 
+// </div> */}
